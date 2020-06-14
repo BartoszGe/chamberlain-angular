e78ae6e9873d97a1e8d514635da9d22cb4eb78b3
Bartosz Gertych <bartosz.gertych@tivo.com>
2020-06-14 09:30:57 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GroceryStoreComponent } from './grocery-store/grocery-store.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GroceryStoreComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/grocery-store/grocery-store.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grocery-store',
+  templateUrl: './grocery-store.component.html',
+  styleUrls: ['./grocery-store.component.scss']
+})
+export class GroceryStoreComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
